31966885b28599db826ac4fcd5a0926f2b09fd67
Caleb Sanford <cis12@duke.edu>
2020-01-28 17:53:49 -0500

    formatted visualizer

--- src/View/Visualizer.java
+++ src/View/Visualizer.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import javax.swing.*;
@@ -16,61 +17,80 @@ import javax.swing.*;
  * Feel free to completely change this code or delete it entirely.
  */
 public class Visualizer extends Application {
-    public static final String TITLE = "Example JavaFX";
-    public static final int WINDOW_WIDTH = 900;
-    public static final int WINDOW_HEIGHT = 500;
 
-    private Scene myScene;
-    private Group myRoot;
+  public static final String TITLE = "Example JavaFX";
+  public static final int WINDOW_WIDTH = 900;
+  public static final int WINDOW_HEIGHT = 500;
 
-    @Override
-    public void start(Stage primaryStage) {
-        myScene = setup();
-        primaryStage.setScene(myScene);
-        primaryStage.setTitle(TITLE);
-        primaryStage.show();
-    }
+  private Scene myScene;
+  private Group myRoot;
 
-    private Scene setup () {
-        myRoot = new Group();
-        addToRoot();
+  @Override
+  public void start(Stage primaryStage) {
+    myScene = setup(primaryStage);
+    primaryStage.setScene(myScene);
+    primaryStage.setTitle(TITLE);
+    primaryStage.show();
+  }
 
-        return new Scene(myRoot, WINDOW_WIDTH, WINDOW_HEIGHT);
-    }
+  private Scene setup(Stage primaryStage) {
+    myRoot = new Group();
+    addToRoot(primaryStage);
 
-    private void addToRoot() {
+    return new Scene(myRoot, WINDOW_WIDTH, WINDOW_HEIGHT);
+  }
 
-        myRoot.getChildren().add(createStartButton());
-    }
+  private void addToRoot(Stage primaryStage) {
 
-    private Button createStartButton () {
-        Button button = new Button("My Label");
-        button.setLayoutX(20);
-        button.setLayoutY(20);
+    myRoot.getChildren().add(createStartButton());
+    myRoot.getChildren().add(createFileButton(primaryStage));
+  }
 
-        EventHandler<ActionEvent> event = e -> start_button_pressed();
+  private Button createStartButton() {
+    Button button = new Button("My Label");
+    button.setLayoutX(20);
+    button.setLayoutY(20);
 
-        button.setOnAction(event);
-        return button;
-    }
+    EventHandler<ActionEvent> event = e -> start_button_pressed();
 
-    private Button createStopButton () {
-        Button button = new Button("Stop");
-        button.setLayoutX(20);
-        button.setLayoutY(20);
+    button.setOnAction(event);
+    return button;
+  }
 
-        EventHandler<ActionEvent> event = e -> stop_button_pressed();
+  private Button createStopButton() {
+    Button button = new Button("Stop");
+    button.setLayoutX(20);
+    button.setLayoutY(20);
 
-        button.setOnAction(event);
-        return button;
-    }
+    EventHandler<ActionEvent> event = e -> stop_button_pressed();
 
-    private void start_button_pressed () {
+    button.setOnAction(event);
+    return button;
+  }
 
-    }
+  private Button createFileButton(Stage primaryStage) {
+    Button button = new Button("Load File");
+    button.setLayoutX(100);
+    button.setLayoutY(100);
 
-    private void stop_button_pressed () {
+    EventHandler<ActionEvent> event = e -> file_button_pressed(primaryStage);
 
-    }
+    button.setOnAction(event);
+    return button;
+  }
+
+  private void file_button_pressed(Stage primaryStage) {
+    FileChooser fileChooser = new FileChooser();
+    fileChooser.setTitle("Open Resource File");
+    fileChooser.showOpenDialog(primaryStage);
+  }
+
+  private void start_button_pressed() {
+
+  }
+
+  private void stop_button_pressed() {
+
+  }
 
 }
