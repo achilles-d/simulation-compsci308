700c24a60c5b65d7262846362501b72384ca1bc3
Achilles Dabrowski <ajd66@duke.edu>
2020-01-28 15:51:16 -0500

    Added instance variables and constructor described in DESIGN_PLAN to Grid class as well as helper method to the constructor, initialize (also described in plan)

--- src/cellsociety/Grid.java
+++ src/cellsociety/Grid.java
@@ -1,6 +1,15 @@
 package cellsociety;
 
 public class Grid {
+    public Cell[][] myCells;
+
+    public Grid(String configFileDir){
+
+    }
+
+    public void initialize(String file){
+        //Set grid size and cell states according to XML file 
+    }
 
 
 }
