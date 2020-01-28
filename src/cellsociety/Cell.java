355329a1464a260fa9e6198ac0402dabe5ae0c63
Achilles Dabrowski <ajd66@duke.edu>
2020-01-28 15:35:07 -0500

    Added instance variables and constructor described in DESIGN_PLAN to Cell class

--- src/cellsociety/Cell.java
+++ src/cellsociety/Cell.java
@@ -1,4 +1,17 @@
 package cellsociety;
 
 public class Cell {
+    public char myState;
+    public Cell cellAbove;
+    public Cell cellBelow;
+    public Cell cellLeft;
+    public Cell cellRight;
+
+    public Cell(char state, Cell above, Cell below, Cell left, Cell right){
+        myState = state;
+        cellAbove = above;
+        cellBelow = below;
+        cellLeft = left;
+        cellRight = right;
+    }
 }
