a756c334dcb21cb07ec1347dc0dbf215580764a5
Achilles Dabrowski <ajd66@duke.edu>
2020-02-02 19:04:09 -0500

    Implemented GameOfLifeGrid.updateCellState()

--- src/cellsociety/GameOfLifeGrid.java
+++ src/cellsociety/GameOfLifeGrid.java
@@ -2,6 +2,9 @@ package cellsociety;
 
 public class GameOfLifeGrid extends Grid{
 
+  public static final int maxAliveNeighbors = 3;
+  public static final int minAliveNeighbors = 2;
+
   public GameOfLifeGrid(String[][] configFileDir){
     super(configFileDir);
   }
@@ -15,12 +18,23 @@ public class GameOfLifeGrid extends Grid{
     }
   }
 
-  public void update(){
-
-  }
-
   protected void updateCellState(int i, int j, Enum[][] gridCopy){
-
+    int aliveCount = 0;
+    for(int a = 0; a < maxCellNeighborCount; a++){
+      if(inBounds(i + deltaX[a],j + deltaY[a]) && (gridCopy[i + deltaX[a]][j + deltaY[a]] == GameOfLifeCell.ALIVE)){
+        aliveCount++;
+      }
+    }
+    if(gridCopy[i][j] == GameOfLifeCell.ALIVE){
+      if((aliveCount > maxAliveNeighbors) || (aliveCount < minAliveNeighbors)){
+        myCells[i][j] = GameOfLifeCell.DEAD;
+      }
+    }
+    else{
+      if(aliveCount == maxAliveNeighbors){
+        myCells[i][j] = GameOfLifeCell.ALIVE;
+      }
+    }
   }
 
 }
