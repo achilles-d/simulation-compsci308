b10ea7df1b884c83dd90c93940a563aab170cf26
Achilles Dabrowski <ajd66@duke.edu>
2020-02-03 17:43:21 -0500

    Implemented PredatorPreyGrid.updateCellState() method with the help of IndexPair objects and private helper methods to handle the different types of cells

--- src/cellsociety/PredatorPreyGrid.java
+++ src/cellsociety/PredatorPreyGrid.java
@@ -1,10 +1,34 @@
 package cellsociety;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class PredatorPreyGrid extends Grid{
 
-    public PredatorPreyGrid(String[][] initConfig){
+    private int myMinFishTurnsToBreed;
+    private int myMaxSharkTurns;
+    private int myMinSharkTurnsToBreed;
+    private int[][] myCellTurnsSurvived;
+    private int[][] myTotalTurnsSurvived;
+
+    /**
+     * Create a grid to run the Game of Life simulation
+     * @param initConfig an array of Strings corresponding to each cell's initial state. "FISH" = cell with fish. "SHARK"
+     *                   = cell with shark. "EMPTY" = empty cell
+     */
+    public PredatorPreyGrid(String[][] initConfig, int minFishTurnsToBreed, int maxSharkTurns, int minSharkTurnsToBreed){
         super(initConfig);
+        myMinFishTurnsToBreed = minFishTurnsToBreed;
+        myMaxSharkTurns = maxSharkTurns;
+        myMinSharkTurnsToBreed = minSharkTurnsToBreed;
     }
+
+    @Override
+    public void initialize(String[][] initConfig){
+        super.initialize(initConfig);
+        myCellTurnsSurvived = new int[myCells.length][myCells[0].length];
+    }
+
 
     protected PredatorPreyCell setCellState(String state){
         if(state.equals(PredatorPreyCell.SHARK.toString())){
@@ -19,11 +43,66 @@ public class PredatorPreyGrid extends Grid{
         }
     }
 
-    public void update(){
-        
+    protected void updateCellState(int i, int j, Enum[][] gridCopy){
+        if(! (gridCopy[i][j] == PredatorPreyCell.EMPTY) ) {
+            myCellTurnsSurvived[i][j]++;
+        }
+
+        if(gridCopy[i][j] == PredatorPreyCell.SHARK){
+            updateSharkCell(i, j, gridCopy);
+        }
+        else if(gridCopy[i][j] == PredatorPreyCell.FISH){
+            updateFishCell(i, j, gridCopy);
+        }
     }
 
-    protected void updateCellState(int i, int j, Enum[][] gridCopy){
+    private void updateFishCell(int i, int j, Enum[][] gridCopy) {
+        ArrayList<IndexPair> emptyCellIndices = findNeighborIndices(i, j, gridCopy, PredatorPreyCell.EMPTY);
+        if(!emptyCellIndices.isEmpty()) {
+            int emptyCellIndex = new Random().nextInt(emptyCellIndices.size());
+            myCells[emptyCellIndices.get(emptyCellIndex).getRow()][emptyCellIndices.get(emptyCellIndex).getCol()] = PredatorPreyCell.FISH;
+            setCellEmpty(i, j);
+            if(myCellTurnsSurvived[emptyCellIndices.get(emptyCellIndex).getRow()][emptyCellIndices.get(emptyCellIndex).getCol()] >= myMinFishTurnsToBreed){
+                myCells[i][j] = PredatorPreyCell.FISH;
+            }
+        }
+    }
 
+    private void updateSharkCell(int i, int j, Enum[][] gridCopy) {
+        myTotalTurnsSurvived[i][j]++;
+        if(myCellTurnsSurvived[i][j] > myMaxSharkTurns){
+            setCellEmpty(i,j);
+        }
+        else {
+            ArrayList<IndexPair> fishCellIndices = findNeighborIndices(i, j, gridCopy, PredatorPreyCell.FISH);
+            if (!fishCellIndices.isEmpty()) {
+                int fishCellIndex = new Random().nextInt(fishCellIndices.size());
+                int newRow = fishCellIndices.get(fishCellIndex).getRow();
+                int newCol = fishCellIndices.get(fishCellIndex).getCol();
+                myCells[newRow][newCol] = PredatorPreyCell.SHARK;
+                myCellTurnsSurvived[newRow][newCol] = 0;
+                if(myTotalTurnsSurvived[newRow][newCol] >= myMinSharkTurnsToBreed){
+                    setCellEmpty(i, j);
+                    myCells[i][j] = PredatorPreyCell.SHARK;
+                }
+            }
+        }
+    }
+
+    private ArrayList<IndexPair> findNeighborIndices(int i, int j, Enum[][] gridCopy, PredatorPreyCell targetCell) {
+        ArrayList<IndexPair> cellIndices = new ArrayList<IndexPair>();
+        for(int a = 0; a < ALT_CELL_NEIGHBOR_COUNT; a++){
+            if(inBounds(i + ALT_DELTA_X[a], j + ALT_DELTA_Y[a]) &&
+                    gridCopy[i + ALT_DELTA_X[a]][j + ALT_DELTA_Y[a]] == targetCell){
+                cellIndices.add(new IndexPair(i + ALT_DELTA_X[a], j + ALT_DELTA_Y[a]));
+            }
+        }
+        return cellIndices;
+    }
+
+    private void setCellEmpty(int i, int j){
+        myCells[i][j] = PredatorPreyCell.EMPTY;
+        myCellTurnsSurvived[i][j] = 0;
+        myTotalTurnsSurvived[i][j] = 0;
     }
 }
