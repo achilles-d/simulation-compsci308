fab7aee32d327c548dc86246b6f7686392a34978
Achilles Dabrowski <ajd66@duke.edu>
2020-02-03 18:57:49 -0500

    Added JavaDoc comments to IndexPair and moved Grid.initialize() to section with other protected methods in the class

--- src/cellsociety/IndexPair.java
+++ src/cellsociety/IndexPair.java
@@ -4,15 +4,26 @@ public class IndexPair {
     private int myRow;
     private int myCol;
 
+    /**
+     * Create store container for a pair of indices of interest in a 2D-grid
+     * @param i the row index to be stored
+     * @param j the column index to be stored
+     */
     IndexPair(int i, int j){
         myRow = i;
         myCol = j;
     }
 
+    /**
+     * @return the row index that is stored
+     */
     public int getRow(){
         return myRow;
     }
 
+    /*
+     * @return the column index that is stored
+     */
     public int getCol(){
         return myCol;
     }
