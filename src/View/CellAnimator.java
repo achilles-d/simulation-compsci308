a2b06d596014372c083fc32b8700a571ed681ae5
Caleb Sanford <cis12@duke.edu>
2020-02-03 21:04:54 -0500

    added comments to working GUI code

--- src/View/CellAnimator.java
+++ src/View/CellAnimator.java
@@ -5,24 +5,37 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 
+/**
+ * Class to store the state of Cells and display those
+ * in the GUI
+ *
+ * @author Caleb Sanford
+ */
 public class CellAnimator {
   private Rectangle myCell;
-  private GridPane myPane;
 
+  /**
+   * Create new instance
+   *
+   * @param pane Pane to draw rectangle on
+   * @param X Grid position of cell
+   * @param Y Grid position of cell
+   * @param width Width in pixels of rectangle
+   * @param height Height in pixels of rectangle
+   * @param color Color of rectangle
+   */
   public CellAnimator (GridPane pane, int X, int Y, int width, int height, Color color){
     myCell = new Rectangle(width, height);
     myCell.setFill(color);
-    myPane = pane;
-    System.out.println(width);
-    System.out.println(height);
-    myPane.add(myCell, X, Y, 1,1);
+    pane.add(myCell, X, Y, 1,1);
   }
 
+  /**
+   * Change the state of an existing cell
+   *
+   * @param color New state color
+   */
   public void changeCellState (Paint color) {
     myCell.setFill(color);
-  }
-
-  public void removeCell () {
-    myPane.getChildren().remove(myCell);
   }
 }
