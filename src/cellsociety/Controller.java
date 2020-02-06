e5b99c74145093a852145f5c1965ba20f5dfcdbd
Cemal Yagcioglu <cy111@duke.edu>
2020-02-06 17:40:48 -0500

    added a small comment

--- src/cellsociety/Controller.java
+++ src/cellsociety/Controller.java
@@ -74,6 +74,35 @@ public class Controller {
         return null;
     }
 
+    private void assignGridDimensions(Document doc) {
+        String widthString = doc.getElementsByTagName("width").item(0).getTextContent();
+        String heightString = doc.getElementsByTagName("height").item(0).getTextContent();
+        GRID_WIDTH = Integer.parseInt(widthString);
+        GRID_HEIGHT = Integer.parseInt(heightString);
+        checkWidthAndHeightValues();
+        cellStatesGrid = new String [GRID_HEIGHT][GRID_WIDTH];
+    }
+
+    private void assignCellStates(Document doc) {
+        NodeList cellList = doc.getElementsByTagName("cell");
+        checkNumberOfCells(cellList.getLength());
+        for(int i=0; i<cellList.getLength(); i++){
+            Node cellNode = cellList.item(i);
+            if(cellNode.getNodeType()==Node.ELEMENT_NODE){
+                Element cellElement = (Element) cellNode;
+                String givenCellStateForThisIndex = cellElement.getElementsByTagName("state").item(0).getTextContent();
+                checkValidityOfCellState(givenCellStateForThisIndex);
+                cellStatesGrid[i/GRID_HEIGHT][i%GRID_WIDTH] = givenCellStateForThisIndex;
+            }
+        }
+    }
+
+    private String getSimulationType(Document doc) {
+        String simulationType = doc.getElementsByTagName("simulation_type").item(0).getAttributes().item(0).getTextContent();
+        checkValidityOfSimulationType(simulationType);
+        return simulationType;
+    }
+
     /**
      * This method uses document read by parseXmlFile
      * to initiate the correct simulation type. For some simulations
@@ -135,42 +164,16 @@ public class Controller {
         return (int) Double.parseDouble(parameter);
     }
 
-    private String getSimulationType(Document doc) {
-        String simulationType = doc.getElementsByTagName("simulation_type").item(0).getAttributes().item(0).getTextContent();
-        checkValidityOfSimulationType(simulationType);
-        return simulationType;
-    }
 
-    private void assignGridDimensions(Document doc) {
-        String widthString = doc.getElementsByTagName("width").item(0).getTextContent();
-        String heightString = doc.getElementsByTagName("height").item(0).getTextContent();
-        GRID_WIDTH = Integer.parseInt(widthString);
-        GRID_HEIGHT = Integer.parseInt(heightString);
-        checkWidthAndHeightValues();
-        cellStatesGrid = new String [GRID_HEIGHT][GRID_WIDTH];
-    }
-
-    private void assignCellStates(Document doc) {
-        //String[][] cellStates = new String[][];
-        NodeList cellList = doc.getElementsByTagName("cell");
-        checkNumberOfCells(cellList.getLength());
-        for(int i=0; i<cellList.getLength(); i++){
-            Node cellNode = cellList.item(i);
-            //System.out.println("Node name for student " +i+ " " +cellNode.getNodeName());
-            if(cellNode.getNodeType()==Node.ELEMENT_NODE){
-                Element cellElement = (Element) cellNode;
-                String givenCellStateForThisIndex = cellElement.getElementsByTagName("state").item(0).getTextContent();
-                checkValidityOfCellState(givenCellStateForThisIndex);
-                cellStatesGrid[i/GRID_HEIGHT][i%GRID_WIDTH] = givenCellStateForThisIndex;
-            }
-        }
-    }
+    //IMPORTANT: 1. ADD TRY AND CATCH FOR READER PARTS
+    //           2. Create xml tests for all of this.
 
     private void checkNumberOfCells(int numberOfCells){
         if(numberOfCells!=GRID_HEIGHT*GRID_WIDTH){
-            //throw error that tells that number of cells doesnot match grid size declared.
+            //throw error that tells that number of cells doesnot match grid size declared. (Or possibly cells are not named correctlu)
         }
     }
+
     private void checkValidityOfCellState(String givenCellState){
         //if(givenCellState is not in acceptableCellStates){
             //throw an error that tells XML contains an illegal cell state
@@ -188,16 +191,17 @@ public class Controller {
             //throw an error saying the value inputs for grid width/height contains an error
         }
     }
+
     private void checkIfValueIsBetweenZeroAndOne(Double valueIn){
         if(valueIn<0 || valueIn>1){
             //throw an error saying "Percentage Parameters are not within 0.0 to 1.0 range
         }
     }
+
     private void checkIfIntegerIsOneOrHigher(int integerInput){
         if(integerInput<=0){
             //throw an error saying invalid Integer input
         }
-
     }
 
 }
